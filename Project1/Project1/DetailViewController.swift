89d9cb891fd937944953929db7bfb448eb845c89
Juan Camilo Bages <jcbages@outlook.com>
2020-05-19 23:42:35 -0700

    add text share

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -41,7 +41,7 @@ class DetailViewController: UIViewController {
             return
         }
 
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [selectedImage!, image], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
